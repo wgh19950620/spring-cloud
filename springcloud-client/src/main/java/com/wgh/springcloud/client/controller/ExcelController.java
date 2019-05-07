f66255a764513bb41536a2e9184ace29155eb52c
王光辉 <wangguanghui@trendytech.com.cn>
2019-05-07 15:20:32 +0800

    excel import

--- springcloud-client/src/main/java/com/wgh/springcloud/client/controller/ExcelController.java
+++ springcloud-client/src/main/java/com/wgh/springcloud/client/controller/ExcelController.java
@@ -1,28 +1,41 @@
 package com.wgh.springcloud.client.controller;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.wgh.springcloud.client.service.UserService;
 import com.wgh.springcloud.commons.domain.ExcelData;
+import com.wgh.springcloud.commons.domain.ResourceResponse;
 import com.wgh.springcloud.commons.domain.User;
+import com.wgh.springcloud.commons.domain.VmDiskRequest;
 import com.wgh.springcloud.commons.util.Export2003ExcelUtils;
 import com.wgh.springcloud.commons.util.Export2007ExcelUtils;
 import com.wgh.springcloud.commons.util.IsExcelVersion;
+import com.wgh.springcloud.commons.util.ReadVirtualMachineExcelData;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * export excel controller
  *
  * @author wangguanghui
  */
-@RestController
+@Controller
 public class ExcelController {
 
     private final static String FILENAME = "用户信息.xlsx";
@@ -30,7 +43,27 @@ public class ExcelController {
     @Autowired
     private UserService userService;
 
+    @RequestMapping("/upload")
+    public String index() {
+        return "upload";
+    }
+
+    /*@RequestMapping(value="/upload")
+    @ResponseBody
+    public ModelAndView upload(){
+        ModelAndView mode = new ModelAndView();
+        mode.setViewName("upload");
+        return mode;
+    }*/
+
+    /**
+     * 导出excel
+     *
+     * @param response HttpServletResponse
+     * @throws Exception exception
+     */
     @RequestMapping(value = "/excel", method = RequestMethod.GET)
+    @ResponseBody
     public void excel(HttpServletResponse response) throws Exception {
         ExcelData data = new ExcelData();
         data.setName("hello");
@@ -64,10 +97,90 @@ public class ExcelController {
          * Export2007ExcelUtils.exportExcel(data, out)
          * out.close()
          */
-        if(IsExcelVersion.isExcel2007(FILENAME)) {
+        if (IsExcelVersion.isExcel2007(FILENAME)) {
             Export2007ExcelUtils.exportExcel(response, FILENAME, data);
-        } else if(IsExcelVersion.isExcel2003(FILENAME)) {
+        } else if (IsExcelVersion.isExcel2003(FILENAME)) {
             Export2003ExcelUtils.exportExcel(response, FILENAME, data);
         }
+    }
+
+    @PostMapping("/upload/vmDisks/excel")
+    @ResponseBody
+    public ResourceResponse uploadVmDiskExcel(@RequestParam("file") MultipartFile file) {
+        ResourceResponse response = new ResourceResponse();
+        ReadVirtualMachineExcelData readExcel = new ReadVirtualMachineExcelData();
+
+        List<VmDiskRequest> vmDisks = readExcel.getVmDiskExcelInfo(file);
+        vmDisks.forEach(System.out::println);
+        response.setData(vmDisks);
+        return response;
+    }
+
+    /**
+     * 导入excel 数据
+     *
+     * @param file 导入文件
+     * @return 解析结果
+     */
+    @PostMapping("/upload/excel")
+    @ResponseBody
+    public ResourceResponse uploadExcel(@RequestParam("file") MultipartFile file) throws Exception {
+        ResourceResponse response = new ResourceResponse();
+        Workbook workbook = null;
+        Sheet sheet = null;
+        List<User> users = new ArrayList<>();
+
+        String fileName = file.getOriginalFilename();
+
+        if (fileName.isEmpty() || file.getSize() == 0) {
+
+            response.saveError("上传失败");
+            return response;
+        }
+
+        try {
+            if (IsExcelVersion.isExcel2007(fileName)) {
+
+                workbook = new XSSFWorkbook(file.getInputStream());
+            } else if (IsExcelVersion.isExcel2003(fileName)) {
+
+                workbook = new HSSFWorkbook(file.getInputStream());
+            }
+
+            if (null != workbook) {
+                sheet = workbook.getSheetAt(0);
+            }
+
+            if (null == sheet) {
+                response.saveError("Excel 表为空");
+                return response;
+            }
+
+            System.out.println(sheet.getLastRowNum());
+
+            for (int i = 1; i < sheet.getLastRowNum() + 1; i++) {
+
+                Row row = sheet.getRow(i);
+
+                if (null == row) {
+                    response.saveError("Excel 无数据");
+                    return response;
+                }
+
+                Integer id = Integer.valueOf(row.getCell(0).getStringCellValue());
+                String name = row.getCell(1).getStringCellValue();
+                Integer age = Integer.valueOf(row.getCell(2).getStringCellValue());
+                String identity = row.getCell(3).getStringCellValue();
+
+                users.add(new User(id, name, age, identity));
+            }
+
+            users.forEach(System.out::println);
+            users.forEach(user -> userService.saveUser(user));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return response;
     }
 }
--- springcloud-client/src/test/java/com/wgh/springcloud/client/SpringcloudClientApplicationTests.java
+++ springcloud-client/src/test/java/com/wgh/springcloud/client/SpringcloudClientApplicationTests.java
@@ -1,14 +1,26 @@
 package com.wgh.springcloud.client;
 
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ResourceUtils;
 import redis.clients.jedis.JedisCluster;
 
 import javax.annotation.Resource;
+
+import com.wgh.springcloud.commons.domain.User;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -32,6 +44,47 @@ public class SpringcloudClientApplicationTests {
         Long incr = jedisCluster.incr(KEY);
         System.out.println(incr);
         System.out.println(aLong);
+    }
+
+    @Test
+    public void excel() throws Exception {
+        List<User> users = new ArrayList<>();
+        //Excel文件
+        XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(ResourceUtils.getFile("classpath:excel/用户信息.xlsx")));
+        //Excel工作表
+        XSSFSheet sheet = wb.getSheetAt(0);
+
+        int totalRows = sheet.getPhysicalNumberOfRows();
+        int totalCells = 0;
+        // 得到Excel的列数(前提是有行数)
+        if (totalRows > 1 && sheet.getRow(0) != null) {
+            totalCells = sheet.getRow(0).getPhysicalNumberOfCells();
+        }
+
+        for (int r = 1; r < totalRows; r++) {
+
+            XSSFRow row = sheet.getRow(r);
+
+            for (int c = 0; c < totalCells; c++) {
+                XSSFCell cell = row.getCell(c);
+                System.out.println("cell: " + cell);
+
+                System.out.println("cellTypeEnum: " + cell.getCellTypeEnum());
+                System.out.println("type: " + cell.getCellType());
+                System.out.println("CellType.NUMERIC.getCode(): " + CellType.NUMERIC.getCode());
+                System.out.println("CellType.NUMERIC: " + CellType.NUMERIC);
+            }
+
+            Integer id = Integer.valueOf(row.getCell(0).getStringCellValue());
+            String name = row.getCell(1).getStringCellValue();
+            Integer age = Integer.valueOf(row.getCell(2).getStringCellValue());
+            String identity = row.getCell(3).getStringCellValue();
+
+            users.add(new User(id, name, age, identity));
+        }
+
+        System.out.println("共有" + users.size() + "条数据：");
+        users.forEach(System.out::println);
     }
 
 }
